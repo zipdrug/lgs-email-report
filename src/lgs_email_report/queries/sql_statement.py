1817a81595e1585ced22561df5a8f3d55116d25a
Dhashnamurthy <AH86992@LC02FC2S2MD6R.US.AD.WELLPOINT.COM>
2022-12-16 10:00:37 -0500

    chg

--- src/lgs_email_report/queries/sql_statement.py
+++ src/lgs_email_report/queries/sql_statement.py
@@ -1,5 +1,5 @@
 CHECK_AUDIT_LEADS_QUERY = '''
-SELECT pn.pharmacy_network_type_id AS Network,p."name" AS Pharmacy,p.npi AS NPI,count(DISTINCT(al.patientID)) AS Members,
+SELECT pn.pharmacy_network_type_id AS Network,p."name" AS Pharmacy,CAST(p.npi AS varchar) AS NPI,count(DISTINCT(al.patientID)) AS Members,
 '' AS Zipcode,1 AS status
 FROM audit_lead al INNER JOIN leads l ON al.patientID = l.patient_id 
 INNER JOIN pharmacies p ON p.id = l.potential_pharmacy_id AND p.deleted_at IS NULL 
@@ -9,7 +9,7 @@ WHERE al.is_success = 'True' AND cast(al.create_dt as date) = '{curr_date}'
 AND al.operation ='reassign'
 GROUP BY pn.pharmacy_network_type_id,p."name",p.npi 
 UNION 
-SELECT pn.pharmacy_network_type_id AS Network, '' AS Pharmacy,0 AS NPI,count(DISTINCT(al.patientID)) AS Members, a.postal_code AS Zipcode
+SELECT pn.pharmacy_network_type_id AS Network, p."name" AS Pharmacy,CAST(p.npi AS varchar) AS NPI,count(DISTINCT(al.patientID)) AS Members, a.postal_code AS Zipcode
 ,2 AS status
 FROM audit_lead al INNER JOIN leads l ON al.patientID = l.patient_id 
 INNER JOIN addresses a ON a.patient_id = l.patient_id 
