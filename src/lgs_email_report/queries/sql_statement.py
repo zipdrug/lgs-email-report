ff3ecb9f180f8d2a6b09ba6b535ea78c659ac5dc
Dhashnamurthy <AH86992@LC02FC2S2MD6R.US.AD.WELLPOINT.COM>
2022-12-21 09:06:57 -0500

    chg

--- src/lgs_email_report/queries/sql_statement.py
+++ src/lgs_email_report/queries/sql_statement.py
@@ -1,7 +1,7 @@
 CHECK_AUDIT_LEADS_SUCCESS_QUERY = '''
-SELECT pn.pharmacy_network_type_id AS Network,p."name" AS Pharmacy,CAST(p.npi AS varchar) AS NPI,count(DISTINCT(al.patientID)) AS Members,
+SELECT pn.pharmacy_network_type_id AS Network,p."name" AS Pharmacy,CAST(p.npi AS varchar) AS NPI,count(DISTINCT(al.patient_id)) AS Members,
 '' AS Zipcode
-FROM audit_lead al INNER JOIN leads l ON al.patientID = l.patient_id 
+FROM audit_leads al INNER JOIN leads l ON al.patient_id = l.patient_id 
 INNER JOIN pharmacies p ON p.id = l.potential_pharmacy_id AND p.deleted_at IS NULL 
 INNER JOIN pharmacies_network pn ON pn.pharmacy_id = p.id AND pn.deleted_at IS NULL 
 INNER JOIN pharmacy_network_types pnt ON pnt.id = pn.pharmacy_network_type_id AND pnt.deleted_at IS NULL 
@@ -11,9 +11,9 @@ GROUP BY pn.pharmacy_network_type_id,p."name",p.npi order by pn.pharmacy_network
 '''
 
 CHECK_AUDIT_LEADS_FAILURE_QUERY = '''
-SELECT pn.pharmacy_network_type_id AS Network,count(DISTINCT(al.patientID)) AS Members, a.postal_code AS Zipcode
+SELECT pn.pharmacy_network_type_id AS Network,count(DISTINCT(al.patient_id)) AS Members, a.postal_code AS Zipcode
 
-FROM audit_lead al INNER JOIN leads l ON al.patientID = l.patient_id 
+FROM audit_leads al INNER JOIN leads l ON al.patient_id = l.patient_id 
 INNER JOIN addresses a ON a.patient_id = l.patient_id 
 INNER JOIN pharmacies p ON p.id = l.potential_pharmacy_id AND p.deleted_at IS NULL 
 INNER JOIN pharmacies_network pn ON pn.pharmacy_id = p.id AND pn.deleted_at IS NULL
